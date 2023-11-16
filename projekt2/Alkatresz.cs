2eff1e919225e137f07b1cc98c8ccff29dd543e3
arpi-Git-Hub <145491756+arpi-Git-Hub@users.noreply.github.com>
2023-11-16 10:10:09 +0100

    Első

---
+++ projekt2/Alkatresz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekt2
+{
+    internal class Alkatresz
+    {
+        string tipus;
+        string nev;
+        string parameter;
+        int ar;
+
+        public Alkatresz() {}
+        public Alkatresz(string tipus, string nev, string parameter, int ar)
+        {
+            this.tipus = tipus;
+            this.nev = nev;
+            this.parameter = parameter;
+            this.ar = ar;
+        }
+
+        public string Tipus { get => tipus; }
+        public string Nev { get => nev; }
+
+        public string Parameter { get => parameter; }
+        public int Ar { get => ar; }
+    }
+}
---
+++ projekt2/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace projekt2
+{
+    internal class Program
+    {
+
+        static List<Alkatresz> alkatreszek;
+
+        static void Beolvasas(string fajlNev)
+        {
+            alkatreszek = new List<Alkatresz>();
+            foreach (var sor in File.ReadAllLines(fajlNev))
+            {
+                var sorElemei = sor.Split(';');
+                string t = sorElemei[0];
+                string n = sorElemei[1];
+                string p = sorElemei[2];
+                int a = int.Parse(sorElemei[3]);             
+                alkatreszek.Add(new Alkatresz(t, n, p, a));
+            }
+        }
+
+        static void Kiirat(string fajlNev)
+        {
+            Console.Write("Idáig bevitt alkatrészek törlése(i/n): ");
+            string valasz1 = Console.ReadLine();
+            if (valasz1 == "i")
+            {
+                File.WriteAllText(fajlNev, "");
+                Console.WriteLine("Alkatrészek törölve.");
+            }
+            else if (valasz1 == "n")
+            {
+                Console.Write("Új alkatrész bevitele (i/n): ");
+            }
+
+            for (int i = 0; i < i + 1; i++)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("ÚJ ALKATRÉSZ");
+                Console.Write("Alkatrész típusa: ");
+                string tipus = Console.ReadLine();
+                Console.Write("Alkatrész neve: ");
+                string nev = Console.ReadLine();
+                Console.Write("Alkatrész paraméter: ");
+                string parameter = Console.ReadLine();
+                Console.Write("Alkatrész ára: ");
+                string ar = Console.ReadLine();
+                File.AppendAllText(fajlNev, $"{tipus};{nev};{parameter};{ar}" + Environment.NewLine);
+                Console.Write("Új alkatrész bevitele (i/n): ");
+                string valasz2 = Console.ReadLine();
+                if (valasz2 == "n")
+                {
+                    break;
+                }
+            }
+
+            
+        }
+
+        static void Main(string[] args)
+        {
+            
+            Kiirat("fajl.txt");
+
+            Beolvasas("fajl.txt");
+
+            Console.WriteLine("\n");
+            Console.WriteLine("KERESÉS");
+            Console.WriteLine("1. Keresés alkatrész típusára");
+            Console.Write("Alkatrész típusa vagy neve: ");
+            string tipusnevBe = Console.ReadLine();
+            foreach(var a in alkatreszek)
+            {
+                if(a.Tipus == tipusnevBe)
+                {
+                    Console.WriteLine($"\nNév: {a.Nev}\nParaméterek: {a.Parameter}\nÁr:{a.Ar}");
+                }
+                else if(a.Nev == tipusnevBe)
+                {
+                    Console.WriteLine($"\nNév: {a.Nev}\nParaméterek: {a.Parameter}\nÁr:{a.Ar}");
+                }
+            }
+        }
+    }
+}
